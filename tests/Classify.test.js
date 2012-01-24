a04bf840d81f43969b91728cef52a3aff4f36407
Couto <lcouto87@gmail.com>
2012-01-24 02:19:03 +0000

    Configure mocha, add boilerplate and fix build

--- tests/Classify.test.js
+++ tests/Classify.test.js
@@ -1,6 +1,8 @@
-var requirejs = require('requirejs');
+var requirejs = require('requirejs'),
+    assert = require('assert');
 
 requirejs.config({
+    baseUrl: __dirname,
     paths : {
         "Trinity/Classify": "../dist/Classify"
     },
@@ -11,5 +13,14 @@ requirejs(['Trinity/Classify'], function (Classify) {
     //foo and bar are loaded according to requirejs
     //config, but if not found, then node's require
     //is used to load the module.
-    console.log(arguments);
+
+    describe('Array', function(){
+      describe('#indexOf()', function(){
+        it('should return -1 when the value is not present', function(){
+            assert([0,1,2].indexOf(1), 1, "should return 1");
+            assert([0,1,2].indexOf(5), 2, "should return -1");
+        });
+      })
+    });
+
 });
