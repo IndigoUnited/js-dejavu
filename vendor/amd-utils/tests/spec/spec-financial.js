0be148914fe90f54e9e70ed34d5f9b43471199ed
André Cruz <andremiguelcruz@msn.com>
2012-01-25 23:53:20 +0000

    Delete amd-utils folder.

--- vendor/amd-utils/tests/spec/spec-financial.js
+++
@@ -1,8 +0,0 @@
-//automatically generated, do not edit!
-//run `node build` instead
-define([
-    'financial/spec-compoundInterest',
-    'financial/spec-npv'
-], function(){
-    //noop
-});
--- vendor/amd-utils/tests/spec/string/spec-sentenceCase.js
+++
@@ -1,9 +0,0 @@
-define(['src/string/sentenceCase'], function (sentenceCase) {
-
-    describe('string/sentenceCase()', function(){
-
-        it('should uppercase first char of each sentence and lowercase others', function(){
-            expect( sentenceCase('lorem Ipsum doLOr. sit amet dolor.') ).toEqual('Lorem ipsum dolor. Sit amet dolor.');
-        });
-    });
-});
