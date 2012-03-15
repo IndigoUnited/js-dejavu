d355ece386808686f0c50d5ac69099b6fb261809
Andre Cruz <andremiguelcruz@msn.com>
2012-03-15 01:36:29 +0000

    Add missing file.

---
+++ src/common/testKeywords.js
@@ -0,0 +1,41 @@
+//>>includeStart('strict', pragmas.strict);
+/*jslint sloppy:true*/
+/*global define*/
+
+define([
+    'Utils/array/difference',
+    'Utils/object/hasOwn'
+], function (
+    difference,
+    hasOwn
+) {
+    var keywords = [
+        '$name', '$extends', '$implements', '$borrows', '$binds',
+        '$statics', '$finals', '$abstracts', '$constants'
+    ];
+
+    /**
+     * Tests if an object contains an unallowed keyword in a given context.
+     *
+     * @param {String} object          The object to verify
+     * @param {Array}  [allowed="[]"]  The list of allowed keywords
+     *
+     * @return {Mixed} False if is ok, or the key that is unallowed.
+     */
+    function testKeywords(object, allowed) {
+
+        var test = allowed ? difference(keywords, allowed) : keywords,
+            x;
+
+        for (x = test.length - 1; x >= 0; x -= 1) {
+            if (hasOwn(object, test[x])) {
+                return test[x];
+            }
+        }
+
+        return false;
+    }
+
+    return testKeywords;
+});
+//>>includeEnd('strict');
