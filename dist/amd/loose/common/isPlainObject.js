b6f4c76dfc083b64e7ce789c2f688490ef300306
Andre Cruz <andremiguelcruz@msn.com>
2012-04-15 01:11:57 +0100

    Add note to ignore jslint warnings regarding .caller and .callee in loose build.

--- dist/amd/loose/common/isPlainObject.js
+++ dist/amd/loose/common/isPlainObject.js
@@ -21,7 +21,7 @@ define([
         var proto = '__proto__',
             key;
 
-        if (obj.nodeType || (obj != null && obj === obj.window)) {
+        if (obj.nodeType || obj === obj.window) {
             return false;
         }
 
@@ -36,10 +36,10 @@ define([
                 return false;
             }
         } catch (e) {
-            return false;   // IE8,9 Will throw exceptions on certain host objects
+            return false;       // IE8,9 Will throw exceptions on certain host objects
         }
 
-        for (key in obj) {}
+        for (key in obj) {}     // Ignore JSLint warning regarding 'empty block'
 
         return key === undefined || hasOwn(obj, key);
     }
