08e39027ef8cee357cfd72a1ad90f1c7bdebb615
André Cruz <andremiguelcruz@msn.com>
2012-07-08 11:14:54 +0100

    Update package.json, fix CS.

--- test/amd/loose.js
+++ test/amd/loose.js
@@ -1,5 +1,5 @@
 /*jslint node:true, sloppy:true*/
-/*globals define*/
+/*global define*/
 
 if (!global.evaluated) {
     require('../util/adapter.js');
@@ -8,4 +8,4 @@ if (!global.evaluated) {
 var path = '../dist/amd/loose';
 global.modules = [path + '/Class.js', path + '/AbstractClass', path + '/Interface', path + '/FinalClass', path + '/instanceOf'];
 global.build = 'amd/loose';
-define(['../functional'], function () { });
+define(['../functional'], function () {});
