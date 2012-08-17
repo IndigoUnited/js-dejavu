8b0e04993c9f790398b895e4ef2ecdede8e551eb
André Cruz <andremiguelcruz@msn.com>
2012-08-17 11:20:34 +0100

    Add TODO, and update test.js/README.md.

--- test/test.js
+++ test/test.js
@@ -1,7 +1,2 @@
-if (!global.evaluated) {
-    require('./util/adapter.js');
-}
-
-// TODO: loose tests are not being run
 require('./amd/strict');
-require('./amd/loose');
+//require('./amd/loose');
